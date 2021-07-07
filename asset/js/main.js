0502cf8ef337458bdb45a6b43ede565b8e8c499d
chytran <chytran@users.noreply.github.com>
2021-07-07 12:14:19 -0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,38 @@
+/*==================== SHOW MENU =============================*/
+const navMenu = document.querySelector(".nav__menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+
+/*======== Menu Show ======*/
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    })
+}
+
+/*========== Menu Hidden ===========*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ===================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.querySelector(".nav__menu");
+    // When we click on each nav__link, it removes the next
+    navMenu.classList.remove("show-menu")
+}
+navLink.forEach(n => n.addEventListener("click", linkAction))
+
+/*================== CHANGE HEADER BACKGROUND COLOR ON SCROLL =====================*/
+function scrollHeader() {
+    const header = document.getElementById("header");
+    // When scroll is greater than 100, add scroll-header class
+    if(this.scrollY >= 600) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader)
